9a244b281ad7bc9be24b0ae09092e19a4d36aa8b
LlamaChow <76419385+LlamaChow@users.noreply.github.com>
2024-06-29 20:22:50 -0500

    Updating Project

--- Source/PracIII/PracIII.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "PracIII.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PracIII, "PracIII" );
